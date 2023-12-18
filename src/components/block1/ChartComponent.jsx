d0dc4ae64f9463a0805d2975878cfd97c3a7ba05
yfdd617235 <yosefdavid91@gmail.com>
2023-12-18 14:14:29 -0500

    ud table and bars

--- src/components/block1/ChartComponent.jsx
+++ src/components/block1/ChartComponent.jsx
@@ -8,7 +8,7 @@ const ChartComponent = () => {
     labels: labels,
     datasets: [{
       label: 'Reports',
-      data: [1, 4, 0, 6, 2, 5],
+      data: [0, 6, 0, 0, 0, 5],
       backgroundColor: [
         'rgba(54, 162, 235, 0.2)',
         'rgba(54, 162, 235, 0.2)',
